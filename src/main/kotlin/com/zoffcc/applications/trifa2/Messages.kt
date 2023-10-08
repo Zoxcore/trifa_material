43bf155d2cc1f148c225388368e2c5671896e86d
zoff99 <zoff99@users.noreply.github.com>
2023-10-08 19:52:51 +0200

    add scrollbar to message view

--- src/main/kotlin/com/zoffcc/applications/trifa2/Messages.kt
+++ src/main/kotlin/com/zoffcc/applications/trifa2/Messages.kt
@@ -1,17 +1,14 @@
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Size
@@ -23,25 +20,31 @@ import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
-internal inline fun Messages(messages: List<Message>) {
+internal fun Messages(messages: List<Message>) {
     val listState = rememberLazyListState()
     if (messages.isNotEmpty()) {
         LaunchedEffect(messages.last()) {
             listState.animateScrollToItem(messages.lastIndex, scrollOffset = 2)
         }
     }
-    LazyColumn(
-        modifier = Modifier.fillMaxSize().padding(start = 4.dp, end = 4.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        state = listState,
-    ) {
-        item { Spacer(Modifier.size(20.dp)) }
-        items(messages, key = { it.id }) {
-            ChatMessage(isMyMessage = it.user == myUser, it)
+    Box(Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(start = 4.dp, end = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            state = listState,
+        ) {
+            item { Spacer(Modifier.size(20.dp)) }
+            items(messages, key = { it.id }) {
+                ChatMessage(isMyMessage = it.user == myUser, it)
+            }
+            item {
+                Box(Modifier.height(70.dp))
+            }
         }
-        item {
-            Box(Modifier.height(70.dp))
-        }
+        VerticalScrollbar(
+            adapter = rememberScrollbarAdapter(listState),
+            modifier = Modifier.fillMaxHeight().align(CenterEnd).width(10.dp) // .background(Color.Red)
+        )
     }
 }
 
@@ -59,9 +62,7 @@ fun UserPic(user: User) {
         }
     }
     Image(
-        modifier = Modifier
-            .size(imageSize.dp)
-            .clip(CircleShape),
+        modifier = Modifier.size(imageSize.dp).clip(CircleShape),
         contentScale = ContentScale.Crop,
         painter = painter,
         contentDescription = "User picture"
