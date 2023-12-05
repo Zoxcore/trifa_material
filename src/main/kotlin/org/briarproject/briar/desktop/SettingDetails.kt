da1a1f8f9fa1c01b58b00d530761814294b8167e
zoff99 <zoff99@users.noreply.github.com>
2023-12-05 12:04:39 +0100

    make settings screen scrollable

--- src/main/kotlin/org/briarproject/briar/desktop/SettingDetails.kt
+++ src/main/kotlin/org/briarproject/briar/desktop/SettingDetails.kt
@@ -20,17 +20,23 @@ package org.briarproject.briar.desktop
 
 import SETTINGS_HEADER_SIZE
 import SnackBarToast
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Switch
@@ -54,6 +60,7 @@ import com.zoffcc.applications.trifa.TrifaToxService.Companion.orma
 import global_prefs
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.briarproject.briar.desktop.ui.VerticallyScrollableArea
 import org.briarproject.briar.desktop.utils.InternationalizationUtils.i18n
 import update_bootstrap_nodes_from_internet
 
@@ -281,11 +288,21 @@ fun SettingDetails()
 }
 
 @Composable
-fun SettingDetail(header: String, content: @Composable (ColumnScope.() -> Unit)) = Column(Modifier.fillMaxSize()) {
-    Row(Modifier.fillMaxWidth().height(SETTINGS_HEADER_SIZE).padding(horizontal = 16.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
-        Text(header, style = MaterialTheme.typography.h4, color = MaterialTheme.colors.onSurface)
-    }
-    content()
+fun SettingDetail(header: String, content: @Composable (ColumnScope.() -> Unit)) =
+    Column(Modifier.fillMaxSize()) {
+        Row(Modifier.fillMaxWidth().height(SETTINGS_HEADER_SIZE).padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween)
+        {
+            Text(header, style = MaterialTheme.typography.h4, color = MaterialTheme.colors.onSurface)
+        }
+        VerticallyScrollableArea { scrollState ->
+            LazyColumn(state = scrollState) {
+                item {
+                    content()
+                }
+            }
+        }
+
 }
 
 @Composable
