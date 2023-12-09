5140ffb2094e06b94ded9726315a7e9b3e4bb611
zoff99 <zoff99@users.noreply.github.com>
2023-12-09 08:26:23 +0100

    update compose.version

--- src/main/kotlin/org/briarproject/briar/desktop/contact/ProfileCircle.kt
+++ src/main/kotlin/org/briarproject/briar/desktop/contact/ProfileCircle.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
