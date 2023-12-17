6def16016afaa7c332ad34ccb42de4031d84afed
zoff99 <zoff99@users.noreply.github.com>
2023-12-17 10:04:25 +0100

    update ffmpeg_av lib java part

--- src/main/java/com/zoffcc/applications/ffmpegav/AVActivity.java
+++ src/main/java/com/zoffcc/applications/ffmpegav/AVActivity.java
@@ -5,7 +5,7 @@ import java.nio.ByteBuffer;
 public class AVActivity {
 
     private static final String TAG = "ffmpegav.AVActivity";
-    static final String Version = "0.99.10";
+    static final String Version = "0.99.11";
 
     public static native String ffmpegav_version();
     public static native String ffmpegav_libavutil_version();
@@ -425,16 +425,33 @@ public class AVActivity {
         catch(Exception e)
         {
         }
+        //
+        //
         ffmpegav_stop_audio_in_capture();
         ffmpegav_stop_video_in_capture();
-
-
-        final int res_aclose = ffmpegav_close_audio_in_device();
+        int res_aclose = ffmpegav_close_audio_in_device();
         Log.i(TAG, "ffmpeg open close audio capture device: " + res_aclose);
-
-        final int res_vclose = ffmpegav_close_video_in_device();
+        int res_vclose = ffmpegav_close_video_in_device();
         Log.i(TAG, "ffmpeg open close video capture device: " + res_vclose);
-
+        //
+        //
+        // test if calling stop and close again does something bad
+        //
+        Log.i(TAG, "ffmpeg ========= stop and close again =========");
+        Log.i(TAG, "ffmpeg ========= stop and close again =========");
+        Log.i(TAG, "ffmpeg ========= stop and close again =========");
+        Log.i(TAG, "ffmpeg ========= stop and close again =========");
+        ffmpegav_stop_audio_in_capture();
+        ffmpegav_stop_video_in_capture();
+        res_aclose = ffmpegav_close_audio_in_device();
+        Log.i(TAG, "ffmpeg open close audio capture device: " + res_aclose);
+        res_vclose = ffmpegav_close_video_in_device();
+        Log.i(TAG, "ffmpeg open close video capture device: " + res_vclose);
+        Log.i(TAG, "ffmpeg ========= stop and close again =========");
+        Log.i(TAG, "ffmpeg ========= stop and close again =========");
+        Log.i(TAG, "ffmpeg ========= stop and close again =========");
+        //
+        //
         try
         {
             Thread.sleep(100);
@@ -445,13 +462,12 @@ public class AVActivity {
 
         // -----------------------
         // -----------------------
-        /*
         final int res_vd2 = ffmpegav_open_video_in_device(vdevice,
-            vsource, 640, 480, 15, 0);
+                vsource, 640, 480, 15, 0);
         Log.i(TAG, "ffmpeg open video capture device: " + res_vd2);
 
         final int res_ad2 = ffmpegav_open_audio_in_device(adevice,
-            asource);
+                asource);
         Log.i(TAG, "ffmpeg open audio capture device: " + res_ad2);
         ffmpegav_start_video_in_capture();
         ffmpegav_start_audio_in_capture();
@@ -466,13 +482,15 @@ public class AVActivity {
         ffmpegav_stop_video_in_capture();
         ffmpegav_close_audio_in_device();
         ffmpegav_close_video_in_device();
-        */
         // -----------------------
         // -----------------------
 
 
         // -----------------------
         // -----------------------
+        Log.i(TAG, "ffmpeg ========= test with empty parameters =========");
+        Log.i(TAG, "ffmpeg ========= test with empty parameters =========");
+        Log.i(TAG, "ffmpeg ========= test with empty parameters =========");
         final int res_vd3 = ffmpegav_open_video_in_device("",
                 "", 640, 480, 30, 0);
         Log.i(TAG, "ffmpeg open video capture device: " + res_vd3);
@@ -495,6 +513,9 @@ public class AVActivity {
         ffmpegav_close_video_in_device();
         // -----------------------
         // -----------------------
+        Log.i(TAG, "ffmpeg ========= all OK =========");
+        Log.i(TAG, "ffmpeg ========= all OK =========");
+        Log.i(TAG, "ffmpeg ========= all OK =========");
 
     }
 
