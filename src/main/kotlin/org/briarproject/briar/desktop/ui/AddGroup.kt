5a0facf9df162b77234f38f3cbf6707e2b7a0ca9
zoff99 <zoff99@users.noreply.github.com>
2024-02-16 15:08:09 +0100

    create new public group

--- src/main/kotlin/org/briarproject/briar/desktop/ui/AddGroup.kt
+++ src/main/kotlin/org/briarproject/briar/desktop/ui/AddGroup.kt
@@ -1,6 +1,7 @@
 package org.briarproject.briar.desktop.ui
 
 import SnackBarToast
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -27,10 +28,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.zoffcc.applications.trifa.HelperGeneric
 import com.zoffcc.applications.trifa.HelperGeneric.update_savedata_file_wrapper
 import com.zoffcc.applications.trifa.HelperGroup.hex_to_bytes
 import com.zoffcc.applications.trifa.MainActivity
+import com.zoffcc.applications.trifa.MainActivity.Companion.tox_group_get_chat_id
 import com.zoffcc.applications.trifa.MainActivity.Companion.tox_group_join
+import com.zoffcc.applications.trifa.MainActivity.Companion.tox_group_new
+import com.zoffcc.applications.trifa.MainActivity.Companion.tox_group_self_set_name
 import com.zoffcc.applications.trifa.RandomNameGenerator
 import com.zoffcc.applications.trifa.TRIFAGlobals
 import com.zoffcc.applications.trifa.TRIFAGlobals.UINT32_MAX_JAVA
@@ -42,9 +47,11 @@ import java.nio.ByteBuffer
 import java.util.*
 
 @Composable
+@Preview
 fun AddGroup() = Box {
     var join_group_id by remember { mutableStateOf("") }
     var join_group_button_enabled by remember { mutableStateOf(false) }
+    var new_public_group_name by remember { mutableStateOf("") }
     Column {
         Row(modifier = Modifier.fillMaxWidth()) {
             Spacer(modifier = Modifier.width(30.dp))
@@ -102,6 +109,7 @@ fun AddGroup() = Box {
                     } else
                     {
                         // some error on joining group
+                        SnackBarToast("Error joining the Group")
                     }
                 }, colors = ButtonDefaults.buttonColors(), enabled = join_group_button_enabled) {
                 Text("join Group")
@@ -146,6 +154,7 @@ fun AddGroup() = Box {
                     } else
                     {
                         // some error on joining group
+                        SnackBarToast("Error joining the Public Information Group")
                     }
                 }, colors = ButtonDefaults.buttonColors()) {
                 Text("join the Public Information Group")
@@ -160,6 +169,81 @@ fun AddGroup() = Box {
             Text(fontWeight = FontWeight.Normal,
                 fontSize = 16.sp,
                 text ="Joining a Tox Public group for the first time can take a long time, even up to 30 minutes")
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+        Row {
+            Spacer(modifier = Modifier.width(50.dp))
+            Text("or")
+        }
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.width(30.dp))
+            TextField(singleLine = true,
+                textStyle = TextStyle(fontSize = 14.sp),
+                modifier = Modifier.padding(0.dp).width(400.dp),
+                colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White),
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.None,
+                    autoCorrect = false,
+                ), value = new_public_group_name, placeholder = {
+                    Text("enter Group Name", fontSize = 13.sp)
+                }, onValueChange = {
+                    new_public_group_name = it
+                })
+            Spacer(modifier = Modifier.width(30.dp))
+            Button(
+                onClick = {
+                    // HINT: create a new public Group
+                    val my_peername = RandomNameGenerator.getFullName(Random())
+                    val new_group_num = tox_group_new(
+                        ToxVars.TOX_GROUP_PRIVACY_STATE.TOX_GROUP_PRIVACY_STATE_PUBLIC.value,
+                        new_public_group_name,
+                        my_peername)
+
+                    update_savedata_file_wrapper()
+
+                    if ((new_group_num >= 0) && (new_group_num < UINT32_MAX_JAVA))
+                    {
+                        // HINT: Group was created
+                        var group_identifier: String = ""
+                        val groupid_buf: ByteBuffer = ByteBuffer.allocateDirect(TRIFAGlobals.GROUP_ID_LENGTH * 2)
+                        if (tox_group_get_chat_id(new_group_num, groupid_buf) == 0)
+                        {
+                            val groupid_buffer = ByteArray(TRIFAGlobals.GROUP_ID_LENGTH)
+                            groupid_buf.clear()
+                            groupid_buf.get(groupid_buffer, 0, TRIFAGlobals.GROUP_ID_LENGTH)
+                            group_identifier = HelperGeneric.bytesToHex(groupid_buffer, 0, TRIFAGlobals.GROUP_ID_LENGTH).lowercase()
+                        }
+
+                        try
+                        {
+                            tox_group_self_set_name(new_group_num, my_peername)
+                            update_savedata_file_wrapper()
+                        } catch (_: Exception)
+                        {
+                        }
+
+                        try
+                        {
+                            val new_privacy_state = MainActivity.tox_group_get_privacy_state(new_group_num)
+                            val group_name = new_public_group_name
+                            val group_num_peers = MainActivity.tox_group_peer_count(new_group_num)
+                            groupstore.add(item = GroupItem(numPeers = group_num_peers.toInt(),
+                                name = group_name!!, isConnected = 0,
+                                groupId = group_identifier.lowercase(),
+                                privacyState = new_privacy_state))
+                        } catch (_: Exception)
+                        {
+                        }
+                        new_public_group_name = ""
+                        SnackBarToast("You created new public Group")
+                    } else
+                    {
+                        // some error on joining group
+                        SnackBarToast("Error creating new public Group")
+                    }
+                }, colors = ButtonDefaults.buttonColors(), enabled = new_public_group_name.isNotEmpty()) {
+                Text("Create new public Group")
+            }
         }
         Spacer(modifier = Modifier.height(30.dp))
     }
