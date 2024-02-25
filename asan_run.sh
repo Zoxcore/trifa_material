01937c312957af11746eb3eb594c40237d1f73b5
zoff99 <zoff99@users.noreply.github.com>
2024-02-25 09:54:47 +0100

    refactor asan run script

--- asan_run.sh
+++ asan_run.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+#####################################
+#
+# config:
+#
+java_17_or_newer_bin=/usr/lib/jvm/java-17-openjdk-amd64/bin/java
+asan_runtime_lib=/usr/lib/x86_64-linux-gnu/libasan.so.6.0.0
+#
+#
+#
+#####################################
+
+
+
 p=$(pwd)
 h=$(echo $HOME)
 
@@ -11,8 +24,12 @@ if [ "$1""x" == "buildx" ]; then
   ./gradlew -Dorg.gradle.java.home=/usr/lib/jvm/java-17-openjdk-amd64/ packageUberJarForCurrentOS
 fi
 
-LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.6.0.0 \
-/usr/lib/jvm/java-17-openjdk-amd64/bin/java -Dapple.awt.application.name=TRIfA \
--Dcom.apple.mrj.application.apple.menu.about.name=TRIfA -Dcompose.application.configure.swing.globals=true -Dcompose.application.resources.dir="$p"/build/compose/tmp/prepareAppResources -Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Duser.variant \
+LD_PRELOAD="$asan_runtime_lib" \
+"$java_17_or_newer_bin" -Dapple.awt.application.name=TRIfA \
+-Dcom.apple.mrj.application.apple.menu.about.name=TRIfA \
+-Dcompose.application.configure.swing.globals=true \
+-Dcompose.application.resources.dir="$p"/build/compose/tmp/prepareAppResources \
+-Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Duser.variant \
 -cp "$p"/build/compose/jars/trifa_material-linux-x64-1.0.22.jar \
 MainKt
+
