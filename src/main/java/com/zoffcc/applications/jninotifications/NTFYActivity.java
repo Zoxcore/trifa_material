029b45052166b5647f39d12aa1779a56f9f0e895
zoff99 <zoff99@users.noreply.github.com>
2024-03-07 17:22:37 +0100

    update JNI lib

--- src/main/java/com/zoffcc/applications/jninotifications/NTFYActivity.java
+++ src/main/java/com/zoffcc/applications/jninotifications/NTFYActivity.java
@@ -154,8 +154,49 @@ public class NTFYActivity {
         String icon_path = icon_file.getAbsolutePath();
 
         Log.i(TAG, "jninotifications version: " + jninotifications_version());
+
+        final int s = 100;
+
         jninotifications_notify("test application",
             "title", "message",
             icon_path);
+        try{Thread.sleep(s);}catch(Exception e){}
+
+        jninotifications_notify(null,
+            null, null,
+            null);
+        try{Thread.sleep(s);}catch(Exception e){}
+
+        jninotifications_notify(null,
+            "1b", "1c",
+            "1d");
+        try{Thread.sleep(s);}catch(Exception e){}
+
+        jninotifications_notify("2a",
+            null, "2c",
+            "2d");
+        try{Thread.sleep(s);}catch(Exception e){}
+
+        jninotifications_notify("3a",
+            "3b", null,
+            "3d");
+        try{Thread.sleep(s);}catch(Exception e){}
+
+        jninotifications_notify("4a",
+            "4b", "4c",
+            null);
+        try{Thread.sleep(s);}catch(Exception e){}
+
+        jninotifications_notify("हिन्दी",
+            "हिन्दी", "हिन्दी",
+            icon_path);
+        try{Thread.sleep(s);}catch(Exception e){}
+
+        jninotifications_notify("iconpathhindi",
+            "हिन्दी", "हिन्दी",
+            "हिन्दी");
+
+
+        // HINT: it should only show 4 notifications (and also not crash)
     }
 }
