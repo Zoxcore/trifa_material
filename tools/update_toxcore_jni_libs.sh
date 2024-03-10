603ef07305c5eee7de092d0efd6c1fbd1adfe8bb
zoff99 <zoff99@users.noreply.github.com>
2024-03-10 15:07:18 +0100

    add scripts to download toxcore JNI libs

---
+++ tools/update_toxcore_jni_libs.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+base_url='https://github.com/zoff99/trifa_material/releases/download/nightly/'
+
+file_mac='libjni-c-toxcore.jnilib'
+file_mac_noise='libjni-c-toxcore_noise.jnilib'
+file_win='jni-c-toxcore.dll'
+file_win_noise='jni-c-toxcore_noise.dll'
+file_lix='libjni-c-toxcore.so'
+file_lix_noise='libjni-c-toxcore_noise.so'
+file_rpi='libjni-c-toxcore_raspi.so'
+
+_HOME2_=$(dirname $0)
+export _HOME2_
+_HOME_=$(cd $_HOME2_;pwd)
+export _HOME_
+
+basedir="$_HOME_""/../resources/common/"
+
+cd "$basedir"
+wget "$base_url""$file_mac" -O "$file_mac"
+wget "$base_url""$file_mac_noise" -O "$file_mac_noise"
+wget "$base_url""$file_win" -O "$file_win"
+wget "$base_url""$file_mac_noise" -O "$file_mac_noise"
+wget "$base_url""$file_lix" -O "$file_lix"
+wget "$base_url""$file_lix_noise" -O "$file_lix_noise"
+wget "$base_url""$file_rpi" -O "$file_rpi"
