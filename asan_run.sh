1e37f777bd0516de0cecee5ac77b96ba9b1e4454
zoff99 <zoff99@users.noreply.github.com>
2024-03-11 16:42:48 +0100

    add option to run without asan but with pure java not gradle

--- asan_run.sh
+++ asan_run.sh
@@ -17,9 +17,11 @@ p=$(pwd)
 h=$(echo $HOME)
 
 if [ "$1""x" == "buildx" ]; then
-  cp -av resources/common/libffmpeg_av_jni.so__ASAN resources/common/libffmpeg_av_jni.so
-  cp -av resources/common/libjni_notifications.so__ASAN resources/common/libjni_notifications.so
-  cp -av resources/common/libjni-c-toxcore.so__ASAN resources/common/libjni-c-toxcore.so
+  if [ "$2""x" != "1x" ]; then
+    cp -av resources/common/libffmpeg_av_jni.so__ASAN resources/common/libffmpeg_av_jni.so
+    cp -av resources/common/libjni_notifications.so__ASAN resources/common/libjni_notifications.so
+    cp -av resources/common/libjni-c-toxcore.so__ASAN resources/common/libjni-c-toxcore.so
+  fi
   ./gradlew -Dorg.gradle.java.home="$java_17_or_newer_dir" packageDistributionForCurrentOS
   ./gradlew -Dorg.gradle.java.home="$java_17_or_newer_dir" packageUberJarForCurrentOS
 fi
@@ -27,12 +29,21 @@ fi
 # options to log GC stats, and limit memusage a lot
 # -XX:+UseG1GC -XX:MinHeapFreeRatio=1 -XX:MaxHeapFreeRatio=1
 
-LD_PRELOAD="$asan_runtime_lib" \
-"$java_17_or_newer_dir"/bin/java -Dapple.awt.application.name=TRIfA \
--Dcom.apple.mrj.application.apple.menu.about.name=TRIfA \
--Dcompose.application.configure.swing.globals=true \
--Dcompose.application.resources.dir="$p"/build/compose/tmp/prepareAppResources \
--Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Duser.variant \
--cp "$p"/build/compose/jars/trifa_material-linux-x64-1.0.24.jar \
-MainKt
-
+if [ "$2""x" != "1x" ]; then
+  LD_PRELOAD="$asan_runtime_lib" \
+    "$java_17_or_newer_dir"/bin/java -Dapple.awt.application.name=TRIfA \
+    -Dcom.apple.mrj.application.apple.menu.about.name=TRIfA \
+    -Dcompose.application.configure.swing.globals=true \
+    -Dcompose.application.resources.dir="$p"/build/compose/tmp/prepareAppResources \
+    -Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Duser.variant \
+    -cp "$p"/build/compose/jars/trifa_material-linux-x64-1.0.24.jar \
+    MainKt
+else
+  "$java_17_or_newer_dir"/bin/java -Dapple.awt.application.name=TRIfA \
+    -Dcom.apple.mrj.application.apple.menu.about.name=TRIfA \
+    -Dcompose.application.configure.swing.globals=true \
+    -Dcompose.application.resources.dir="$p"/build/compose/tmp/prepareAppResources \
+    -Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Duser.variant \
+    -cp "$p"/build/compose/jars/trifa_material-linux-x64-1.0.24.jar \
+    MainKt
+fi
