981e0af39e1b8f4ffb9a064b4b6f66e865f3b072
zoff99 <zoff99@users.noreply.github.com>
2024-04-08 17:59:43 +0200

    add tools to remove emoji from TTF (doesnt really seem to make anything better)

---
+++ tools/python_remove_emoji_from_ttf_font.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import re
+import fontforge
+from sys import argv
+
+matcher = re.compile(r'^([0-9A-F]+) ;')
+
+ttf = fontforge.open(argv[1])
+for line in open('emoji-data.txt'):
+    match = matcher.match(line)
+    try:
+        if match:
+            val = int(match[1], 16)
+            ttf.removeGlyph(val)
+    except ValueError:
+        pass
+ttf.generate(argv[1])
