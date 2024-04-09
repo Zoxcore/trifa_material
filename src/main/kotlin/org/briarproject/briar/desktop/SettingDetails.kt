702e01b51d7606fd86d1f70921801775e6255eca
zoff99 <zoff99@users.noreply.github.com>
2024-04-09 21:36:39 +0200

    do not use custom emoji font as default, there seem to be some issues still

--- src/main/kotlin/org/briarproject/briar/desktop/SettingDetails.kt
+++ src/main/kotlin/org/briarproject/briar/desktop/SettingDetails.kt
@@ -331,12 +331,12 @@ private fun database_settings()
 private fun general_settings()
 {
     // ---- use custom font that has color emoji AND normal text ----
-    var use_custom_font_with_color_emoji by remember { mutableStateOf(true) }
+    var use_custom_font_with_color_emoji by remember { mutableStateOf(false) }
     try
     {
-        if (!global_prefs.getBoolean("main.use_custom_font_with_color_emoji", true))
+        if (global_prefs.getBoolean("main.use_custom_font_with_color_emoji", false))
         {
-            use_custom_font_with_color_emoji = false
+            use_custom_font_with_color_emoji = true
         }
     } catch (_: Exception)
     {
