fd2e914760edfefd4a7438d6edd90068a1717a9e
zoff99 <zoff99@users.noreply.github.com>
2024-06-09 11:08:56 +0200

    create pull requests for dependencies

---
+++ tools/check_libvpx.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+r1='https://github.com/webmproject/libvpx'
+ver=$(git ls-remote --refs --sort='v:refname' --tags "$r1" \
+    | cut --delimiter='/' --fields=3 | grep -v '\-' | tail --lines=1)
+
+echo "$ver"
+
+cd ../circle_scripts/
+grep -ril 'VPX_VERSION='|xargs -L1 sed -i -e 's#VPX_VERSION=".*"#VPX_VERSION="'"$ver"'"#'
+
