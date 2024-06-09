e8fecd1a195e621958bd070b9d6e15c7d810dec6
zoff99 <zoff99@users.noreply.github.com>
2024-06-09 13:51:02 +0200

    check also x265 dep

---
+++ tools/check_x265.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+r1='https://bitbucket.org/multicoreware/x265_git.git'
+ver=$(git ls-remote --sort=v:refname "$r1" refs/heads/stable \
+    |awk '{print $1}' | tail --lines=1)
+
+echo "$ver"
+
+cd ./circle_scripts/
+grep -ril '_X265_VERSION_='|xargs -L1 sed -i -e 's#_X265_VERSION_=".*"#_X265_VERSION_="'"$ver"'"#'
+
