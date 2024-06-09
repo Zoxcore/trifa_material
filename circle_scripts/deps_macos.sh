19c2c0dfd4d7e170a723a2a30b1ce0cea6a8415e
zoff99 <6833516+zoff99@users.noreply.github.com>
2024-06-09 11:54:06 +0000

    update x264

--- circle_scripts/deps_macos.sh
+++ circle_scripts/deps_macos.sh
@@ -46,7 +46,7 @@ FFMPEG_VERSION="n7.0.1"
 OPUS_VERSION="v1.5.2"
 SODIUM_VERSION="1.0.20"
 VPX_VERSION="v1.14.1"
-_X264_VERSION_="7ed753b10a61d0be95f683289dfb925b800b0676"
+_X264_VERSION_="31e19f92f00c7003fa115047ce50978bc98c3a0d"
 # ------- deps verisions ---------
 
 # ---------- ffmpeg ---------
