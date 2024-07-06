94e3808f9a0ebed43b06d6a5586fd89160d331c2
zoff99 <zoff99@users.noreply.github.com>
2024-07-06 11:01:32 +0200

    add actual version info into title and commit message

--- tools/check_compose.sh
+++ tools/check_compose.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 r1='https://github.com/JetBrains/compose-multiplatform'
-ver=$(git ls-remote --refs --sort='v:refname' --tags "$r1" \
-    | cut --delimiter='/' --fields=3 | tail -1|sed -e 's#^v##')
+ver=$(git ls-remote --refs --sort='v:refname' --tags "$r1" 2>/dev/null \
+    | cut --delimiter='/' --fields=3 2>/dev/null| tail -1 2>/dev/null|sed -e 's#^v##' 2>/dev/null)
 
-echo "$ver"
+echo "__VERSIONUPDATE__:""$ver"
 
 sed -i -e 's#^compose.version=.*$#compose.version='"$ver"'#' gradle.properties
 
