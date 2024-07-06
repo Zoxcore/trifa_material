4f31b41093832173822b98ae47d414306108aa19
zoff99 <zoff99@users.noreply.github.com>
2024-07-06 11:16:54 +0200

    add actual version info into title and commit message

--- tools/check_opus.sh
+++ tools/check_opus.sh
@@ -4,7 +4,7 @@ r1='https://github.com/xiph/opus'
 ver=$(git ls-remote --refs --sort='v:refname' --tags "$r1" \
     | cut --delimiter='/' --fields=3 | grep -v '\-' | tail --lines=1)
 
-echo "$ver"
+echo "__VERSIONUPDATE__:""$ver"
 
 cd ./circle_scripts/
 grep -ril 'OPUS_VERSION='|xargs -L1 sed -i -e 's#OPUS_VERSION=".*"#OPUS_VERSION="'"$ver"'"#'
