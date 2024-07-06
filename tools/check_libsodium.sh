46e818290d246b27f1bc4a5be3072ed9a03aa2bd
zoff99 <zoff99@users.noreply.github.com>
2024-07-06 11:16:54 +0200

    add actual version info into title and commit message

--- tools/check_libsodium.sh
+++ tools/check_libsodium.sh
@@ -4,7 +4,7 @@ r1='https://github.com/jedisct1/libsodium'
 ver=$(git ls-remote --refs --sort='v:refname' --tags "$r1" \
     | cut --delimiter='/' --fields=3 | grep '\-RELEASE' | tail --lines=1|sed -e 's#-RELEASE$##')
 
-echo "$ver"
+echo "__VERSIONUPDATE__:""$ver"
 
 cd ./circle_scripts/
 grep -ril 'SODIUM_VERSION='|xargs -L1 sed -i -e 's#SODIUM_VERSION=".*"#SODIUM_VERSION="'"$ver"'"#'
--- tools/check_libvpx.sh
+++ tools/check_libvpx.sh
@@ -4,7 +4,7 @@ r1='https://github.com/webmproject/libvpx'
 ver=$(git ls-remote --refs --sort='v:refname' --tags "$r1" \
     | cut --delimiter='/' --fields=3 | grep -v '\-' | tail --lines=1)
 
-echo "$ver"
+echo "__VERSIONUPDATE__:""$ver"
 
 cd ./circle_scripts/
 grep -ril 'VPX_VERSION='|xargs -L1 sed -i -e 's#VPX_VERSION=".*"#VPX_VERSION="'"$ver"'"#'
