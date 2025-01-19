46e190e748f137037e9566219ef3c79da20c8161
zoff99 <zoff99@users.noreply.github.com>
2025-01-19 09:54:11 +0100

    silence linter

--- src/main/kotlin/org/briarproject/briar/desktop/contact/GrouppeerItemView.kt
+++ src/main/kotlin/org/briarproject/briar/desktop/contact/GrouppeerItemView.kt
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+@file:Suppress("FunctionName", "SpellCheckingInspection", "CascadeIf", "LocalVariableName", "ReplaceIsEmptyWithIfEmpty", "ReplaceSizeCheckWithIsNotEmpty")
 
 package org.briarproject.briar.desktop.contact
 
