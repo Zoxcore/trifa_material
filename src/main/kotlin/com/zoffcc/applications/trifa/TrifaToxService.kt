f327c586d66cdc3868b951012a0d941fc8c625f0
zoff99 <zoff99@users.noreply.github.com>
2025-08-14 21:37:08 +0200

    trigger push before FT

--- src/main/kotlin/com/zoffcc/applications/trifa/TrifaToxService.kt
+++ src/main/kotlin/com/zoffcc/applications/trifa/TrifaToxService.kt
@@ -1,4 +1,4 @@
-@file:Suppress("ConvertToStringTemplate", "LocalVariableName")
+@file:Suppress("ConvertToStringTemplate", "LocalVariableName", "ReplaceSizeCheckWithIsNotEmpty")
 
 package com.zoffcc.applications.trifa
 
@@ -281,6 +281,7 @@ class TrifaToxService
                                     while (ii.hasNext())
                                     {
                                         val m_resend_ft = ii.next()
+                                        friend_call_push_url(m_resend_ft.tox_friendpubkey, m_resend_ft.sent_timestamp)
                                         if (tox_friend_get_connection_status(
                                                 tox_friend_by_public_key(m_resend_ft.tox_friendpubkey))
                                             != ToxVars.TOX_CONNECTION.TOX_CONNECTION_NONE.value)
