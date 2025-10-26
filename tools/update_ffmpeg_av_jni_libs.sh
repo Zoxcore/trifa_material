8a66fc676465fb4dbf777f1e033b3abb5ed70056
zoff99 <zoff99@users.noreply.github.com>
2025-10-26 13:04:43 +0100

    update update scripts

--- tools/update_ffmpeg_av_jni_libs.sh
+++ tools/update_ffmpeg_av_jni_libs.sh
@@ -9,6 +9,7 @@ file_asan_lix='libffmpeg_av_jni.so__ASAN'
 file_rpi='libffmpeg_av_jni_raspi.so'
 
 java_source_url_01='https://raw.githubusercontent.com/zoff99/ffmpeg_av_jni/refs/heads/master/com/zoffcc/applications/ffmpegav/AVActivity.java'
+java_source_url_02='https://raw.githubusercontent.com/zoff99/ffmpeg_av_jni/refs/heads/master/com/zoffcc/applications/ffmpegav/Log.java'
 
 _HOME2_=$(dirname $0)
 export _HOME2_
@@ -27,4 +28,5 @@ wget "$base_url""$file_rpi" -O "$file_rpi"
 
 cd "$_HOME_"/../
 wget "$java_source_url_01" -O ./src/main/java/com/zoffcc/applications/ffmpegav/AVActivity.java
+wget "$java_source_url_02" -O ./src/main/java/com/zoffcc/applications/ffmpegav/Log.java
 
--- tools/update_jni_notifications_jni_libs.sh
+++ tools/update_jni_notifications_jni_libs.sh
@@ -7,14 +7,8 @@ file_asan_lix='libjni_notifications.so__ASAN'
 file_lix_rust='libjni_notifications_rs.so'
 file_rpi='libjni_notifications_raspi.so'
 
-
-47 KB
-2 days ago
-
-39 KB
-2 days ago
-
-
+java_source_url_01='https://raw.githubusercontent.com/zoff99/jni_notifications/refs/heads/master/com/zoffcc/applications/jninotifications/NTFYActivity.java'
+java_source_url_02='https://raw.githubusercontent.com/zoff99/jni_notifications/refs/heads/master/com/zoffcc/applications/jninotifications/Log.java'
 
 _HOME2_=$(dirname $0)
 export _HOME2_
@@ -29,3 +23,8 @@ wget "$base_url""$file_lix" -O "$file_lix"
 wget "$base_url""$file_asan_lix" -O "$file_asan_lix"
 wget "$base_url""$file_lix_rust" -O "$file_lix_rust"
 wget "$base_url""$file_rpi" -O "$file_rpi"
+
+cd "$_HOME_"/../
+wget "$java_source_url_01" -O ./src/main/java/com/zoffcc/applications/jninotifications/NTFYActivity.java
+wget "$java_source_url_02" -O ./src/main/java/com/zoffcc/applications/jninotifications/Log.java
+
