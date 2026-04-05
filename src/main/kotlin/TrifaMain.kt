3d0fd715e700287747b6210b5165d7b683b73590
zoff99 <zoff99@users.noreply.github.com>
2026-04-05 17:13:18 +0200

    use "isActive". it would work with "true" also but be on the safe side

--- src/main/kotlin/TrifaMain.kt
+++ src/main/kotlin/TrifaMain.kt
@@ -200,6 +200,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
@@ -479,7 +480,7 @@ fun App()
                                     }
                                     var online_button_text by remember { mutableStateOf("offline") }
                                     LaunchedEffect(online_button_text_wrapper) {
-                                        while (true) {
+                                        while (isActive) {
                                             if (online_button_text != online_button_text_wrapper) {
                                                 online_button_text = online_button_text_wrapper
                                             }
