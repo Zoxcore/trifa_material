959b4c1b46bb60dced49553bc94e319cba552bbb
zoff99 <zoff99@users.noreply.github.com>
2026-04-15 10:03:10 +0200

    jetbrains maven server seems to have changed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://packages.jetbrains.team/maven/p/cmp/dev")
         google()
     }
 
