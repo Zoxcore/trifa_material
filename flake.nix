2b40333736188092aba4add12b7f329fbc9bb08d
kaefert <kaefert@gmail.com>
2026-08-16 13:55:51 +0200

    nixos jdk26 dependencies

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "Development environment";
+  description = "Development environment with Skiko support";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11"; # old nixos stable repo
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -20,35 +20,44 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+
+        jdk = pkgs-unstable.temurin-bin-26;
+
+        # Group all the libraries together
+        runtimeLibs = with pkgs; [
+          stdenv.cc.cc.lib # <-- 1. This provides libstdc++.so.6
+          libGL
+          libX11
+          libXcursor
+          libXrandr
+          libXrender
+          libXext
+          libXxf86vm
+          fontconfig
+          freetype
+          libv4l
+          libnotify
+          libxcb
+          libpulseaudio
+          alsa-lib
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            #maven
-            jdk25
-            #pkgs-unstable.temurin-bin-26
-
-            libGL
-            libX11
-            libXcursor
-            libXrandr
-            libXrender
-            libXext
-            libXxf86vm
-            fontconfig
-            freetype
-            libv4l
-            libnotify
-            libxcb
-            libpulseaudio
-            alsa-lib
-          ];
+            jdk
+          ] ++ runtimeLibs;
 
           shellHook = ''
-            export PATH="${pkgs.jdk25}/bin:$PATH"
-            #export PATH="${pkgs-unstable.temurin-bin-26}/bin:$PATH"
+            export JAVA_HOME=${jdk}
+            export PATH="$JAVA_HOME/bin:$PATH"
             export _JAVA_OPTIONS="-Dskiko.linux.autodpi=false -Dsun.java2d.uiScale=2"
-            export LD_LIBRARY_PATH="${pkgs.libGL}/lib:${pkgs.libX11}/lib:${pkgs.libXcursor}/lib:${pkgs.libXrandr}/lib:${pkgs.libXrender}/lib:${pkgs.libXext}/lib:${pkgs.libXxf86vm}/lib:${pkgs.fontconfig}/lib:${pkgs.freetype}/lib:${pkgs.libv4l}/lib:${pkgs.libnotify}/lib:${pkgs.libxcb}/lib:${pkgs.libpulseaudio}/lib:${pkgs.alsa-lib}/lib:$LD_LIBRARY_PATH"
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"
+            
+            # This feeds nix-ld the libraries Skiko tries to dynamically load at runtime
+            export NIX_LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}"
+            
+            echo "Java 26 & Skiko environment loaded cleanly via nix-ld!"
           '';
         };
       }
