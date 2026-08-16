63c7e267933656ff08eab2c038513febcb660de6
kaefert <kaefert@gmail.com>
2026-08-16 13:26:51 +0200

    nixos-26.05 + jdk25 working

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "Development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,26 +12,29 @@
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
       flake-utils,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            maven
-            jdk21
+            #maven
+            jdk25
+            #pkgs-unstable.temurin-bin-26
 
             libGL
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXrender
-            xorg.libXext
-            xorg.libXxf86vm
+            libX11
+            libXcursor
+            libXrandr
+            libXrender
+            libXext
+            libXxf86vm
             fontconfig
             freetype
             libv4l
@@ -41,9 +45,10 @@
           ];
 
           shellHook = ''
-            export PATH="${pkgs.jdk21}/bin:$PATH"
+            export PATH="${pkgs.jdk25}/bin:$PATH"
+            #export PATH="${pkgs-unstable.temurin-bin-26}/bin:$PATH"
             export _JAVA_OPTIONS="-Dskiko.linux.autodpi=false -Dsun.java2d.uiScale=2"
-            export LD_LIBRARY_PATH="${pkgs.libGL}/lib:${pkgs.xorg.libX11}/lib:${pkgs.xorg.libXcursor}/lib:${pkgs.xorg.libXrandr}/lib:${pkgs.xorg.libXrender}/lib:${pkgs.xorg.libXext}/lib:${pkgs.xorg.libXxf86vm}/lib:${pkgs.fontconfig}/lib:${pkgs.freetype}/lib:${pkgs.libv4l}/lib:${pkgs.libnotify}/lib:${pkgs.libxcb}/lib:${pkgs.libpulseaudio}/lib:${pkgs.alsa-lib}/lib:$LD_LIBRARY_PATH"
+            export LD_LIBRARY_PATH="${pkgs.libGL}/lib:${pkgs.libX11}/lib:${pkgs.libXcursor}/lib:${pkgs.libXrandr}/lib:${pkgs.libXrender}/lib:${pkgs.libXext}/lib:${pkgs.libXxf86vm}/lib:${pkgs.fontconfig}/lib:${pkgs.freetype}/lib:${pkgs.libv4l}/lib:${pkgs.libnotify}/lib:${pkgs.libxcb}/lib:${pkgs.libpulseaudio}/lib:${pkgs.alsa-lib}/lib:$LD_LIBRARY_PATH"
           '';
         };
       }
