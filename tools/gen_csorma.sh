24afb05946e78d8ae42a61b61a2f5b196374f055
zoff99 <zoff99@users.noreply.github.com>
2026-08-16 09:17:17 +0200

    update sorma generate script

--- tools/gen_csorma.sh
+++ tools/gen_csorma.sh
@@ -18,6 +18,32 @@ echo "#############"
 echo "#############"
 echo ""
 echo "have a look in ${basedir}/sorma2/gen/com/zoffcc/applications/sorm/ for the generated java source files"
-echo ""
+echo "and ${basedir}/src/main/java/com/zoffcc/applications/sorm/ for the files used in the application"
 echo "#############"
 echo "#############"
+
+files="
+BootstrapNodeEntryDB.java
+Column.java
+ConferenceDB.java
+ConferenceMessage.java
+FileDB.java
+Filetransfer.java
+FriendList.java
+GroupDB.java
+GroupMessage.java
+Index.java
+Log.java
+Message.java
+Nullable.java
+OnConflict.java
+PrimaryKey.java
+RelayListDB.java
+Table.java
+TRIFADatabaseGlobalsNew.java
+"
+
+for i in $files ; do
+    cp -v "$basedir"/sorma2/gen/com/zoffcc/applications/sorm/"$i" "$basedir"/src/main/java/com/zoffcc/applications/sorm/
+done
+
