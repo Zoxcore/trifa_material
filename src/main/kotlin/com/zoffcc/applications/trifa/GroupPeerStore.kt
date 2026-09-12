f28414aa57cde67921caedfa84f33026c7beda5f
zoff99 <zoff99@users.noreply.github.com>
2026-09-12 10:22:07 +0200

    [AIassisted] sort peer list for groups in a more useful way

--- src/main/kotlin/com/zoffcc/applications/trifa/GroupPeerStore.kt
+++ src/main/kotlin/com/zoffcc/applications/trifa/GroupPeerStore.kt
@@ -2,12 +2,10 @@
 
 package com.zoffcc.applications.trifa
 
-import androidx.compose.ui.text.toLowerCase
 import global_semaphore_grouppeerlist_ui
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 import org.briarproject.briar.desktop.contact.GroupPeerItem
 
 data class StateGroupPeers(val grouppeers: List<GroupPeerItem> = emptyList(),
@@ -88,71 +86,71 @@ fun CoroutineScope.createGroupPeerStore(): GroupPeerStore
         override fun add(item: GroupPeerItem)
         {
             //launch {
-                @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-                global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
-                var found = false
-                state.grouppeers.forEach {
-                    if (item.pubkey == it.pubkey)
-                    { // friend already in list
-                        found = true
+            @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+            global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
+            var found = false
+            state.grouppeers.forEach {
+                if (item.pubkey == it.pubkey)
+                { // friend already in list
+                    found = true
+                }
+            }
+            if (!found)
+            {
+                var new_peers: ArrayList<GroupPeerItem> = ArrayList()
+                new_peers.addAll(state.grouppeers)
+                new_peers.forEach { item2 ->
+                    if (item2.pubkey == item.pubkey)
+                    {
+                        new_peers.remove(item2)
                     }
                 }
-                if (!found)
-                {
-                    var new_peers: ArrayList<GroupPeerItem> = ArrayList()
-                    new_peers.addAll(state.grouppeers)
-                    new_peers.forEach { item2 ->
-                        if (item2.pubkey == item.pubkey)
-                        {
-                            new_peers.remove(item2)
-                        }
-                    }
-                    new_peers.add(item)
-                    val self_group_pubkey = MainActivity.tox_group_self_get_public_key(
-                        HelperGroup.tox_group_by_groupid__wrapper(item.groupID.lowercase()))
-                    new_peers = getListWithGroupingAndSorting(new_peers, self_group_pubkey)
-                    mutableStateFlow.value = state.copy(grouppeers = new_peers)
-                }
-                global_semaphore_grouppeerlist_ui.release()
+                new_peers.add(item)
+                val self_group_pubkey = MainActivity.tox_group_self_get_public_key(
+                    HelperGroup.tox_group_by_groupid__wrapper(item.groupID.lowercase()))
+                new_peers = getListWithGroupingAndSorting(new_peers, self_group_pubkey)
+                mutableStateFlow.value = state.copy(grouppeers = new_peers)
+            }
+            global_semaphore_grouppeerlist_ui.release()
             //}
         }
 
         override fun remove(item: GroupPeerItem)
         {
             //launch {
-                @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-                global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
-                var sel_pubkey = state.selectedGrouppeerPubkey
-                var sel_item = state.selectedGrouppeer
-                var new_peers: ArrayList<GroupPeerItem> = ArrayList()
-                new_peers.addAll(state.grouppeers)
-                var to_remove_item: GroupPeerItem? = null
-                state.grouppeers.forEach {
-                    if (item.pubkey == it.pubkey)
+            @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+            global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
+            var sel_pubkey = state.selectedGrouppeerPubkey
+            var sel_item = state.selectedGrouppeer
+            var new_peers: ArrayList<GroupPeerItem> = ArrayList()
+            new_peers.addAll(state.grouppeers)
+            var to_remove_item: GroupPeerItem? = null
+            state.grouppeers.forEach {
+                if (item.pubkey == it.pubkey)
+                {
+                    if (state.selectedGrouppeerPubkey == it.pubkey)
                     {
-                        if (state.selectedGrouppeerPubkey == it.pubkey)
+                        sel_pubkey = null
+                        sel_item = null
+                    }
+                    new_peers.forEach { item2 ->
+                        if (item2.pubkey == it.pubkey)
                         {
-                            sel_pubkey = null
-                            sel_item = null
-                        }
-                        new_peers.forEach { item2 ->
-                            if (item2.pubkey == it.pubkey)
-                            {
-                                to_remove_item = item2
-                            }
+                            to_remove_item = item2
                         }
                     }
                 }
-                if (to_remove_item != null)
-                {
-                    new_peers.remove(to_remove_item)
-                }
-                val self_group_pubkey = MainActivity.tox_group_self_get_public_key(
-                    HelperGroup.tox_group_by_groupid__wrapper(item.groupID.lowercase()))
-                new_peers = getListWithGroupingAndSorting(new_peers, self_group_pubkey)
-                mutableStateFlow.value = state.copy(grouppeers = new_peers,
-                    selectedGrouppeer = sel_item, selectedGrouppeerPubkey = sel_pubkey)
-                global_semaphore_grouppeerlist_ui.release()
+            }
+            if (to_remove_item != null)
+            {
+                new_peers.remove(to_remove_item)
+            }
+            val self_group_pubkey = MainActivity.tox_group_self_get_public_key(
+                HelperGroup.tox_group_by_groupid__wrapper(item.groupID.lowercase()))
+            new_peers = getListWithGroupingAndSorting(new_peers, self_group_pubkey)
+            mutableStateFlow.value = state.copy(grouppeers = new_peers,
+                selectedGrouppeer = sel_item, selectedGrouppeerPubkey = sel_pubkey)
+            global_semaphore_grouppeerlist_ui.release()
             //}
         }
 
@@ -162,64 +160,64 @@ fun CoroutineScope.createGroupPeerStore(): GroupPeerStore
             @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
             global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
             var wanted_contact_item: GroupPeerItem? = null
-                state.grouppeers.forEach {
-                    if (pubkey == it.pubkey)
-                    {
-                        wanted_contact_item = it
-                    }
-                }
-                var used_pubkey = pubkey
-                if (wanted_contact_item == null)
+            state.grouppeers.forEach {
+                if (pubkey == it.pubkey)
                 {
-                    used_pubkey = null
+                    wanted_contact_item = it
                 }
-                mutableStateFlow.value = state.copy(grouppeers = state.grouppeers,
-                    selectedGrouppeerPubkey = used_pubkey, selectedGrouppeer = wanted_contact_item)
-                global_semaphore_grouppeerlist_ui.release()
+            }
+            var used_pubkey = pubkey
+            if (wanted_contact_item == null)
+            {
+                used_pubkey = null
+            }
+            mutableStateFlow.value = state.copy(grouppeers = state.grouppeers,
+                selectedGrouppeerPubkey = used_pubkey, selectedGrouppeer = wanted_contact_item)
+            global_semaphore_grouppeerlist_ui.release()
             //}
         }
 
         override fun update(item: GroupPeerItem)
         {
             //launch {
-                @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-                global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
-                var update_item: GroupPeerItem? = null
-                state.grouppeers.forEach {
-                    if (item.pubkey == it.pubkey)
+            @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+            global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
+            var update_item: GroupPeerItem? = null
+            state.grouppeers.forEach {
+                if (item.pubkey == it.pubkey)
+                {
+                    update_item = it.copy()
+                }
+            }
+            if (update_item != null)
+            {
+                var new_peers: ArrayList<GroupPeerItem> = ArrayList()
+                new_peers.addAll(state.grouppeers)
+                var to_remove_item: GroupPeerItem? = null
+                new_peers.forEach { item2 ->
+                    if (item2.pubkey == update_item!!.pubkey)
                     {
-                        update_item = it.copy()
+                        to_remove_item = item2
                     }
                 }
-                if (update_item != null)
+                if (to_remove_item != null)
                 {
-                    var new_peers: ArrayList<GroupPeerItem> = ArrayList()
-                    new_peers.addAll(state.grouppeers)
-                    var to_remove_item: GroupPeerItem? = null
-                    new_peers.forEach { item2 ->
-                        if (item2.pubkey == update_item!!.pubkey)
-                        {
-                            to_remove_item = item2
-                        }
-                    }
-                    if (to_remove_item != null)
-                    {
-                        new_peers.remove(to_remove_item)
-                    }
-                    new_peers.add(item)
-                    val self_group_pubkey = MainActivity.tox_group_self_get_public_key(
-                        HelperGroup.tox_group_by_groupid__wrapper(item.groupID.lowercase()))
-                    new_peers = getListWithGroupingAndSorting(new_peers, self_group_pubkey)
-                    mutableStateFlow.value = state.copy(grouppeers = new_peers,
-                        selectedGrouppeerPubkey = state.selectedGrouppeerPubkey,
-                        selectedGrouppeer = state.selectedGrouppeer)
-                } else
-                {
-                    mutableStateFlow.value = state.copy(grouppeers = (state.grouppeers + item),
-                        selectedGrouppeerPubkey = state.selectedGrouppeerPubkey,
-                        selectedGrouppeer = state.selectedGrouppeer)
+                    new_peers.remove(to_remove_item)
                 }
-                global_semaphore_grouppeerlist_ui.release()
+                new_peers.add(item)
+                val self_group_pubkey = MainActivity.tox_group_self_get_public_key(
+                    HelperGroup.tox_group_by_groupid__wrapper(item.groupID.lowercase()))
+                new_peers = getListWithGroupingAndSorting(new_peers, self_group_pubkey)
+                mutableStateFlow.value = state.copy(grouppeers = new_peers,
+                    selectedGrouppeerPubkey = state.selectedGrouppeerPubkey,
+                    selectedGrouppeer = state.selectedGrouppeer)
+            } else
+            {
+                mutableStateFlow.value = state.copy(grouppeers = (state.grouppeers + item),
+                    selectedGrouppeerPubkey = state.selectedGrouppeerPubkey,
+                    selectedGrouppeer = state.selectedGrouppeer)
+            }
+            global_semaphore_grouppeerlist_ui.release()
             //}
         }
 
@@ -268,11 +266,11 @@ fun CoroutineScope.createGroupPeerStore(): GroupPeerStore
         override fun clear()
         {
             //launch {
-                @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-                global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
-                mutableStateFlow.value = state.copy(grouppeers = emptyList(),
-                    selectedGrouppeerPubkey = null, selectedGrouppeer = null)
-                global_semaphore_grouppeerlist_ui.release()
+            @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+            global_semaphore_grouppeerlist_ui.acquire((Throwable().stackTrace[0].fileName + ":" + Throwable().stackTrace[0].lineNumber))
+            mutableStateFlow.value = state.copy(grouppeers = emptyList(),
+                selectedGrouppeerPubkey = null, selectedGrouppeer = null)
+            global_semaphore_grouppeerlist_ui.release()
             //}
         }
     }
@@ -281,13 +279,17 @@ fun CoroutineScope.createGroupPeerStore(): GroupPeerStore
 val rolesOrder = mapOf(0 to 0, 1 to 1, 2 to 2, 3 to 3)
 
 fun getListWithGroupingAndSorting(peerlist: ArrayList<GroupPeerItem>, self_group_pubkey: String?)
-    : ArrayList<GroupPeerItem>
+        : ArrayList<GroupPeerItem>
 {
     val selfOrder = mapOf(true to 0, false to 1)
+
+    // Map online status: online -> 0, offline -> 1.
+    val onlineOrder = mapOf(true to 0, false to 1)
+
     return ArrayList(peerlist.sortedWith(
-        compareBy<GroupPeerItem> { selfOrder[it.pubkey == self_group_pubkey] }.
-        thenBy { rolesOrder[it.peerRole] }.
-        thenBy { it.name.lowercase() }
-    )
-    )
+        compareBy<GroupPeerItem> { selfOrder[it.pubkey == self_group_pubkey] } // 1. Myself always first
+            .thenBy { onlineOrder[it.connectionStatus != ToxVars.TOX_CONNECTION.TOX_CONNECTION_NONE.value] }   // 2. Online peers vs Offline peers
+            .thenBy { rolesOrder[it.peerRole] }                                // 3. Founder -> Moderator -> User -> Observer
+            .thenBy { it.name?.lowercase() ?: "" }                             // 4. Alphabetical sorting within the smaller groups
+    ))
 }
