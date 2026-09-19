e1e1bd599991e6bac5ecd2e8667eb23ae69270e4
zoff99 <zoff99@users.noreply.github.com>
2026-09-19 17:00:29 +0200

    fix script

--- tools/find_non_ascii_chars_in_source.sh
+++ tools/find_non_ascii_chars_in_source.sh
@@ -13,8 +13,7 @@ cd src/
 final_rc=0
 for i in $(find . -type f \( -name "*.kt" -o -name "*.java" \) 2>/dev/null | sort 2>/dev/null) ; do
     "$basedir"/tools/check_nonascii.sh -a \
-      0939,093F,0928,094D,0926,0940,1F44D,1F44E,2764,FE0F,263A,00F6,00E4,00FC,00DF,00B0, \
-      2265,2501 \
+      0939,093F,0928,094D,0926,0940,1F44D,1F44E,2764,FE0F,263A,00F6,00E4,00FC,00DF,00B0,2265,2501 \
       "$i"
     rc=$?
     if [ "$rc""x" != "0x" ]; then
